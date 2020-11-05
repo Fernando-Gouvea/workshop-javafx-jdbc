f6b08010baad3fe806804f7be722cfbee50889e6
Fernando-Gouvea <fernando.gouvea.lfg@gmail.com>
2020-11-05 16:41:22 -0300

    MenuBar

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,14 @@
 package application;
 	
+
+
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.ScrollPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
@@ -17,8 +17,12 @@ public class Main extends Application {
 
 		try {
 			FXMLLoader loader =  new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
-			Parent parent = loader.load();
-			Scene mainScene = new Scene(parent);
+			ScrollPane scrollPane = loader.load();
+			
+			scrollPane.setFitToHeight(true);
+			scrollPane.setFitToWidth(true);
+			
+			Scene mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
